fe07a23d09bb9b16a14d5b0167e8dabbd1c4fff9
Jorgnavarro <jorgluisnavarro@gmail.com>
2023-11-28 21:09:37 -0500

    exercises-3.7-3.8

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
 
 app.disable('x-powered-by')
 
 const PORT = 3001;
+
+morgan.token('body', (req) => JSON.stringify(req.body))
+
+app.use(morgan(':method :url :status :res[content-length] -response-time ms :body'))
 
 let persons = [
     {
