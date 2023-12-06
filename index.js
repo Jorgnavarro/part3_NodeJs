9489e0d05088f9d59426c76c8cdedab462803d9b
Jorgnavarro <jorgluisnavarro@gmail.com>
2023-12-05 19:00:50 -0500

    completing-CRUD-exercises-3.12-3.18

--- index.js
+++ index.js
@@ -17,34 +17,6 @@ morgan.token('body', (req) => JSON.stringify(req.body))
 
 app.use(morgan(':method :url :status :res[content-length] :response-time ms :body'))
 
-let persons = [
-    {
-        "name": "Arto Hellas",
-        "number": "040-123456",
-        "id": 1
-    },
-    {
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523",
-        "id": 2
-    },
-    {
-        "name": "Dan Abramov",
-        "number": "12-43-234345",
-        "id": 3
-    },
-    {
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122",
-        "id": 4
-    },
-    {
-        "name": "Kakaroto Powerfull",
-        "number": "787878",
-        "id": 5
-    }
-]
-
 
 
 // welcome page
@@ -54,12 +26,18 @@ app.get('/', (req, res) => {
 // info about: amount of contacts and date
 app.get('/info', (req, res) => {
     const date = new Date()
-    res.send(`
-    <div>
-        <h1>Phonebook has info for ${persons.length} people</h1>
+    Person.find({}).then(personToSearch => {
+        const arr = [...personToSearch];
+        return arr.length
+    }).then(totalContacts => {
+        res.send(`
+        <div>
+        <h1>Phonebook has info for ${totalContacts} people</h1>
         <p>${date}<p>
-    </div>
-    `)
+        </div>
+        `)
+    })
+    
 })
 // obtain the list of the persons
 app.get('/api/persons', (req, res) => {
@@ -68,37 +46,37 @@ app.get('/api/persons', (req, res) => {
     })
 })
 // obtain an especific information about the contact with id
-app.get('/api/persons/:id', (req, res) => {
+app.get('/api/persons/:id', (req, res, next) => {
     // const id = Number(req.params.id)
     // const person = persons.find(p => p.id === id);
     // if(person){
-    //     res.json(person)
-    // }else{
-    //     res.status(404).send(`<h1>The person with id ${id}, doesn't exist in our database</h1>`);
-    // }
-    Person.findById(req.params.id)
-        .then(person => {
-            res.json(person)
+        //     res.json(person)
+        // }else{
+            //     res.status(404).send(`<h1>The person with id ${id}, doesn't exist in our database</h1>`);
+            // }
+            Person.findById(req.params.id).then(person => {
+                if(person){
+                    res.json(person)
+                }else{
+                    res.status(404).send('<h1>The resource does not exist in our database</h1>')
+                }
+            })
+            .catch(err => {
+                console.log(`The person with id: ${req.params.id} does not exist.`)
+                next(err)
+            })
+            
         })
-        .catch(err => {
-            console.log(`The person with id: ${req.params.id} does not exist.`)
-        })
-
-})
-
+        
 app.use(express.json())
+        
 
-//function generate id
-// const generateId = () => {
-//     const lastNumberID = 10000
-//     return Math.floor(Math.random() * (lastNumberID - 5 + 1) + 5);
-// }
 
 
 //create a new contact
 app.post('/api/persons', (req, res) => {
     const body = req.body
-    if(body.name === undefined || body.number === undefined){
+    if(!body.name||!body.number){
         return res.status(400).json({
             error: "content missing"
         })
@@ -124,14 +102,44 @@ app.post('/api/persons', (req, res) => {
     })
 })
 
-// delete a contact
-app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id);
-    persons = persons.filter(person => person.id !== id);
-    res.status(204).end()
+// Update a contact
+app.put('/api/persons/:id', (req, res, next) => {
+    const body = req.body
+
+    const person = {
+        name: body.name,
+        number: body.number,
+    }
+
+    Person.findByIdAndUpdate(req.params.id, person, {new: true})
+        .then(updatedPerson =>{
+            res.json(updatedPerson)    
+        })
+        .catch(error => {
+            next(error)
+        })
 })
 
-const PORT = process.env.PORT || 3001;
+// delete a contact
+app.delete('/api/persons/:id', (req, res, next) => {
+    Person.findByIdAndDelete(req.params.id)
+        .then(result => {
+            res.status(204).end()
+        })
+        .catch(err => next(err))
+})
+
+const errorHandler = (err, req, res, next) => {
+    console.log(err.message)
+    if(err.name === "CastError"){
+        return res.status(400).send({error: 'malformatted id'})
+    }
+}
+
+app.use(errorHandler)
+
+
+const PORT = process.env.PORT;
 
 app.listen(PORT)
 
